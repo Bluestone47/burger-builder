074354cf0bc90d9b6c823ad78059a541ce388776
xiandong <dongxian.bruce@gmail.com>
2019-09-27 10:05:58 +1000

    fix navigation items css

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 import classes from './NavigationItems.module.css';
 
 const navigationItems = (props) => (
-  <ul className={classes.navigationItems}>
+  <ul className={classes.NavigationItems}>
     <NavigationItem link='' active>Burger Builder</NavigationItem>
     <NavigationItem link=''>Checkout</NavigationItem>
   </ul>
