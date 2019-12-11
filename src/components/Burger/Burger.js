2f4857634fdda3bb881b56cf2e9ce3d72e2a2a35
xiandong <dongxian.bruce@gmail.com>
2019-12-11 18:06:25 +1100

    add router

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import { withRouter } from 'react-router-dom';
 
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 
 const burger = (props) => {
@@ -26,4 +27,4 @@ const burger = (props) => {
   )
 }
 
-export default burger;
+export default withRouter(burger);
