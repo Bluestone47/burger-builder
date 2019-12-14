1a2ffa9308f29c5cf72024a8c3289924994b268f
xiandong <dongxian.bruce@gmail.com>
2019-12-14 21:24:42 +1100

    use hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useEffect, Suspense } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
 
-function App() {
+const Checkout = React.lazy(() => {
+  return import('./containers/Checkout/Checkout');
+})
+
+const Orders = React.lazy(() => {
+  return import('./containers/Orders/Orders');
+})
+
+const App = () => {
+
+  let routes = (
+    <Switch>
+      <Route path='/checkout' render={() => <Checkout />} />
+      <Route path='/orders' render={() => <Orders />} />
+      <Route path='/' exact component={BurgerBuilder} />
+      <Redirect to='/' />
+    </Switch>
+  )
+
   return (
     <div>
       <Layout>
-        <Switch>
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/orders' component={Orders} />
-          <Route path='/' exact component={BurgerBuilder} />
-        </Switch>
+        <Suspense fallback={<p>Loading</p>}>
+          {routes}
+        </Suspense>
       </Layout>
     </div>
   );
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Aux from '../Auxiliary/Auxiliary';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
@@ -6,34 +6,30 @@ import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.module.css';
 
-class Layout extends Component {
-  state = {
-    showSideDrawer: false
-  }
-  
-  sideDrawerClosedHandler = () => {
-    this.setState({ showSideDrawer: false })
+const Layout = props => {
+
+  const [sideDrawerVisible, setSideDrawerVisible] = useState(false)
+
+  const sideDrawerClosedHandler = () => {
+    setSideDrawerVisible(false)
   }
 
-  sideDrawerToggleHandler = () => {
-    this.setState((prevState) => {
-      return {showSideDrawer: !prevState.showSideDrawer}
-    })
+  const sideDrawerToggleHandler = () => {
+    setSideDrawerVisible(!sideDrawerVisible)
   }
 
-  render() {
-    return (
-      <Aux>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
-        <SideDrawer
-          open={this.state.showSideDrawer}
-          closed={this.sideDrawerClosedHandler} />
-        <main className={classes.Content}>
-          {this.props.children}
-        </main>
-      </Aux>
-    )
-  }
+  return (
+    <Aux>
+      <Toolbar drawerToggleClicked={sideDrawerToggleHandler} />
+      <SideDrawer
+        open={sideDrawerVisible}
+        closed={sideDrawerClosedHandler} />
+      <main className={classes.Content}>
+        {props.children}
+      </main>
+    </Aux>
+  )
+
 }
 
 export default Layout;
