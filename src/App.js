6ba2daa2844b680f88946d48b3f9675f1063727e
xiandong <dongxian.bruce@gmail.com>
2019-12-14 21:32:22 +1100

    use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
